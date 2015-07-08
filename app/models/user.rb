242b17e0a5bf0521b6829374bcb29e543a36244e
adam & alex <student@epicodus.com>
2015-07-08 15:33:38 -0700

    add styling. add controllers thru devise instead of custom

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "missing.png"
+         validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 1.megabytes
-  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :listings
-  devise_for :users
-  resources :users
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :users, :only => [:show]
+  resources :cancels, :only => [:index]
   root "listings#index"
 end
